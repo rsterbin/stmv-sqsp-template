b0801f36b9886984cc4fb32584a44586c9e4fd1d
Reha Sterbin <reha@omniti.com>
2016-04-01 22:17:33 -0400

    Unholy hack to make the blog summary block look like the design

--- scripts/site.js
+++ scripts/site.js
@@ -2,3 +2,12 @@
  * Custom js that might be needed anywhere in the site
  */
 
+$(document).ready(function () {
+    // Trick the homepage Angelus summary into thinking the read more link is a button
+    $('#homeAngelus a.summary-read-more-link')
+        .text('Read More')
+        .addClass('button')
+        .addClass('hollow')
+        .wrap('<div class="read-more"></div>');
+});
+
