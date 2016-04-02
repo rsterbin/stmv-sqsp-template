fb9232bb45ad76db6dff7fbd14a4b2f503166be5
Reha Sterbin <reha@omniti.com>
2016-04-01 22:43:55 -0400

    More tweaks for the Angelus block

--- scripts/site.js
+++ scripts/site.js
@@ -9,5 +9,8 @@ $(document).ready(function () {
         .addClass('button')
         .addClass('hollow')
         .wrap('<div class="read-more"></div>');
+    // Kill any hardcoded font sizes in the summary
+    $('#homeAngelus .summary-excerpt p[style*="font-size"]').removeAttr('style');
+    $('#homeAngelus .summary-excerpt span[style*="font-size"]').removeAttr('style');
 });
 
