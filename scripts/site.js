75ab10a253767a6f3c13987866cd6bb8170ab454
Reha Sterbin <reha@omniti.com>
2016-04-07 22:59:29 -0400

    Set up a script area for liturgical calendar stuff

--- scripts/site.js
+++ scripts/site.js
@@ -2,6 +2,28 @@
  * Custom js that might be needed anywhere in the site
  */
 
+// {{{ Liturgical calendar handling
+
+var LC = {
+    calendarUrl: '/liturgical-calendar/',
+    months: [ 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december' ],
+
+    paginationLinks: function (cdate) {
+        var prev = new Date(),
+            next = new Date();
+        prev.setMonth(cdate.getMonth() - 1);
+        next.setMonth(cdate.getMonth() + 1);
+        return {
+            prev: calendarUrl + '?month=' + this.months[prev.getMonth()] + '-' + prev.getYear(),
+            next: calendarUrl + '?month=' + this.months[next.getMonth()] + '-' + next.getYear()
+        };
+    }
+
+};
+
+
+// }}}
+
 $(document).ready(function () {
     // Trick the homepage Angelus summary into thinking the read more link is a button
     $('#homeAngelus a.summary-read-more-link')
