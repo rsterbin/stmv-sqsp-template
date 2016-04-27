3be19f1363f4e116aa2e19ae34d01983b1359a94
Reha Sterbin <reha@omniti.com>
2016-04-27 08:16:42 -0400

    Debugging for liturgical calendar colors on angelus

--- scripts/site.js
+++ scripts/site.js
@@ -136,7 +136,9 @@ var LC = {
         for (var i = 0; i < services.length; i++) {
             var item = services[i];
             var $service = $(this.serviceTemplate);
+            console.log(item.tags);
             for (var j; j < item.tags.length; j++) {
+                console.log(item.tags[j]);
                 $service.addClass(item.tags[j]);
             }
             var idate = new Date(item.startDate);
