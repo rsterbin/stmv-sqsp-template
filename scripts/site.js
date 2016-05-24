6a130a43f055dbbb80076e3f6b470ceefac4b8e0
Reha Sterbin <reha@omniti.com>
2016-05-23 21:18:51 -0400

    Redesigned homepage

--- scripts/site.js
+++ scripts/site.js
@@ -166,15 +166,6 @@ var LC = {
 // }}}
 
 $(document).ready(function () {
-    // Trick the homepage Angelus summary into thinking the read more link is a button
-    $('#homeAngelus a.summary-read-more-link')
-        .text('Read More')
-        .addClass('button')
-        .addClass('hollow')
-        .wrap('<div class="read-more"></div>');
-    // Kill any hardcoded font sizes in the summary
-    $('#homeAngelus .summary-excerpt p[style*="font-size"]').removeAttr('style');
-    $('#homeAngelus .summary-excerpt span[style*="font-size"]').removeAttr('style');
     LC.initWeekly();
 });
 
